daa6313a83afc00c506b37286547d9732bf3417b
= <Selenac99@hotmail.com>
2024-04-09 12:39:13 -0300

    terminando proyecto

---
+++ src/components/TaskList.jsx
@@ -0,0 +1,38 @@
+import PropTypes from "prop-types";
+import TaskCard from "./TaskCard";
+import { useContext } from "react";
+import { TaskContext } from "../context/TaskContext";
+
+function TaskList() {
+  const { tasks } = useContext(TaskContext);
+
+  if (tasks.length === 0) {
+    return (
+      <h1 className="text-white text-4xl font-bold text-center">
+        No hay tareas aún
+      </h1>
+    );
+  }
+
+  return (
+    <div className="grid grid-cols-4 gap-2">
+      {tasks.map((task) => (
+        <TaskCard key={task.id} task={task} />
+      ))}
+    </div>
+  );
+}
+
+// Adding prop types validation
+TaskList.propTypes = {
+  tasks: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      title: PropTypes.string.isRequired,
+      description: PropTypes.string.isRequired,
+    })
+  ).isRequired,
+  deleteTask: PropTypes.func.isRequired, // Adding deleteTask prop validation
+};
+
+export default TaskList;
